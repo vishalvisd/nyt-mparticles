edcba777c1a60821b65d663f0e9eb4986576e682
Vishal Daga <vishaldaga@XE-UAE-IT-00027.local>
2022-04-15 08:17:33 +0530

    adding service worker build entry point

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,18 @@ export default defineConfig({
     silent: true,
     setupFiles: './setupTests.ts',
   },
+  build: {
+    rollupOptions: {
+      input: {
+        // the default entry point
+        app: './index.html',
+        'sw': './sw.js',
+      },
+      output: {
+        entryFileNames: assetInfo => {
+          return assetInfo.name === 'sw' ? '[name].js' : 'assets/js/[name]-[hash].js'
+        }
+      },
+    },
+  }
 })
